642ecaaed4096672f6901bb9c8e19543fdcf5410
amansuthar19hmh <sutharbarduaaman@gmail.com>
2024-07-13 16:19:28 +0530

    Login and SignUp done with Firebase

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,52 +1,60 @@
-import 'package:blog_application/components/blocks.dart';
-import 'package:blog_application/components/buttons.dart';
-import 'package:blog_application/components/custom_textfields.dart';
-import 'package:blog_application/services/google_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:blog_application/components/blocks.dart';
+import 'package:blog_application/services/google_auth.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  // Handle login button click
-  _loginButton() async {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String _errorMessage = '';
+
+  void _login() async {
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+
     try {
-      final FirebaseAuth auth = FirebaseAuth.instance;
       final UserCredential userCredential =
-          await auth.signInWithEmailAndPassword(
-              email: _emailController.text, password: _passwordController.text);
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
 
-      // Navigate to home page or display success message
+      // Navigate to another screen or show success message
       if (userCredential.user != null) {
-        // Replace with your navigation logic
-        Navigator.pushNamed(context, '/dashboard'); // Assuming a home route
+        // Example: Navigator.pushNamed(context, '/home');
+        print('Login successful for: ${userCredential.user!.email}');
       }
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        // Display error message to user
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Invalid email or password'),
-          ),
-        );
-      } else if (e.code == 'wrong-password') {
-        // Display error message to user
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Invalid email or password'),
-          ),
-        );
-      }
-    } catch (e) {}
+      setState(() {
+        switch (e.code) {
+          case 'user-not-found':
+            _errorMessage = 'No user found for that email.';
+            break;
+          case 'wrong-password':
+            _errorMessage = 'Wrong password provided for that user.';
+            break;
+          case 'invalid-email':
+            _errorMessage = 'The email address is not valid.';
+            break;
+          default:
+            _errorMessage = 'An error occurred. Please try again later.';
+        }
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'An unexpected error occurred. Please try again later.';
+      });
+    }
   }
 
   @override
@@ -56,81 +64,48 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SingleChildScrollView(
         child: Center(
           child: Column(
-            children: [
+            children: <Widget>[
+              const SizedBox(height: 20),
               const Image(
                 image: AssetImage('assets/Login.jpg'),
-                fit: BoxFit.contain,
+                fit: BoxFit.fill,
               ),
-              const SizedBox(height: 10),
-              Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      CustomTextfields(
-                        hintText: "UserMail",
-                        obscureText: false,
-                        Icons: Icons.mail,
-                        controller: _emailController,
-                      ),
-                      const SizedBox(height: 10),
-                      CustomTextfields(
-                        hintText: "Password",
-                        obscureText: true,
-                        Icons: Icons.lock,
-                        controller: _passwordController,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                Navigator.pushNamed(context, "/Forgetpassword");
-                              },
-                              child: const Text(
-                                "forget password?",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Buttons(
-                        text: "SignIn",
-                        color: Colors.black,
-                        textColor: Colors.white,
-                        onPressed: () {
-                          // Check if _formKey is initialized and has a current state
-                          if (_formKey.currentState != null) {
-                            if (_formKey.currentState!.validate()) {
-                              _loginButton();
-                            }
-                          } else {
-                            // Handle the error gracefully
-                          }
-                        },
-                      ),
-                    ],
-                  )),
+              const SizedBox(height: 20),
+              _buildErrorMessage(),
+              _buildTextField(
+                controller: _emailController,
+                hintText: 'Email',
+                icon: Icons.mail,
+              ),
+              _buildTextField(
+                controller: _passwordController,
+                hintText: 'Password',
+                icon: Icons.lock,
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.black,
+                ),
+              ),
               const SizedBox(height: 10),
               const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 52),
                 child: Row(
-                  children: [
+                  children: <Widget>[
                     Expanded(
                       child: Divider(
                         thickness: 0.5,
                         color: Colors.white,
                       ),
                     ),
-                    Padding(
+                    const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 8.0),
                       child: Text(
-                        "Or",
+                        'Or',
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
@@ -146,36 +121,67 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children: <Widget>[
                   Blocks(
                     ontap: () => GoogleAuth().signInWithGoogle(),
-                    imagepath: "assets/Google.png",
+                    imagepath: 'assets/Google.png',
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("New User?"),
+                children: <Widget>[
+                  const Text('New User?'),
                   InkWell(
-                    onTap: () => {
-                      Navigator.pushNamed(context, '/Signup'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup');
                     },
                     child: const Text(
-                      "Signup",
+                      'Signup',
                       style: TextStyle(
                         color: Color.fromARGB(255, 6, 88, 32),
                       ),
                     ),
-                  )
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    required IconData icon,
+    bool obscureText = false,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          hintText: hintText,
+          prefixIcon: Icon(icon),
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildErrorMessage() {
+    if (_errorMessage.isEmpty) {
+      return Container();
+    }
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        _errorMessage,
+        style: TextStyle(color: Colors.red),
       ),
     );
   }
